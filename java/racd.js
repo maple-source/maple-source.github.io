09fbf4838448a2de4b2a7d3aa834692028b538d0
maplefennic <mjswolf0@gmail.com>
2025-03-31 14:00:14 -0700

    tabs for racd

---
+++ java/racd.js
@@ -0,0 +1,10 @@
+//the tabs system is adapted directly from W3: https://www.w3schools.com/howto/howto_js_tabs.asp
+function display(section) {
+    var content = document.getElementsByClassName("content");
+    console.log("hideing content")
+    for ( var i = 0; i < content.length; i++) {
+      content[i].style.display = "none";
+    }
+    console.log("display " + section)
+    document.getElementById(section).style.display = "block";
+  }
