f34ebe0c0ea302db45688fccd2baf3462ed46e1e
maplefennic <mjswolf0@gmail.com>
2025-04-01 16:44:28 -0700

    fix

--- java/racd.js
+++ java/racd.js
@@ -1,7 +1,7 @@
 //the tabs system is adapted directly from W3: https://www.w3schools.com/howto/howto_js_tabs.asp
 function display(section) {
     var content = document.getElementsByClassName("content");
-    console.log("hideing content")
+    console.log("hiding content")
     for ( var i = 0; i < content.length; i++) {
       content[i].style.display = "none";
     }
